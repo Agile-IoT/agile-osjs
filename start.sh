f057c5e1676d2cac1b29ab7d54aac0dab0aac891
Csaba Kiraly <csaba.kiraly@gmail.com>
2017-05-12 15:34:05 +0200

    user AGILE_HOST env var for redirection URL

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 if [ ! -f /etc/configured ]; then
-   GW_HOST=`cat /etc/hostname | xargs echo -n`
-   grunt config -set=client.Connection.RedirectIDM --value=http://$GW_HOST.local:3000/oauth2/dialog/authorize/
+   GW_HOST=${AGILE_HOST:-`cat /etc/hostname | xargs echo -n`.local}
+   grunt config -set=client.Connection.RedirectIDM --value=http://$GW_HOST:3000/oauth2/dialog/authorize/
    grunt
    echo "var $GW_HOST"
    echo "var $GW_HOST"

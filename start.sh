29aad9fa2007d8b2e5f6d673605923f44daf7d0b
dp <dp>
2017-12-30 14:31:47 +0100

    modify start script to re-configure whenever AGILE_HOST or /etc/hostname changes
    Change-Type: patch

--- start.sh
+++ start.sh
@@ -5,13 +5,16 @@
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
-# 
+#
 # Contributors:
 #     Create-Net / FBK - initial API and implementation
 #-------------------------------------------------------------------------------
 
-if [ ! -f /etc/configured ]; then
-   GW_HOST=${AGILE_HOST:-`cat /etc/hostname | xargs echo -n`.local}
+HOST=`cat /etc/configured`
+GW_HOST=${AGILE_HOST:-`cat /etc/hostname | xargs echo -n`.local}
+
+if [ "$GW_HOST" != "$HOST" ]
+then
    if [ "$AGILE_SSL" == "1" ]
    then
        PROTO="https"
@@ -25,7 +28,8 @@ if [ ! -f /etc/configured ]; then
    echo "value for AGILE_SSL $AGILE_SSL"
    echo "OS-JS configured to use IDM at: $PROTO://$GW_HOST:$PORT/oauth2/dialog/authorize/"
    echo "var $GW_HOST"
-   touch /etc/configured
+   echo "$GW_HOST">/etc/configured
 fi
+
 
 ./bin/start-dev.sh
